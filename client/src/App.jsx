dda80baa4e63f579521bdbca160d53cd555260a6
Samy <dev.exelight@gmail.com>
2022-01-20 15:37:10 +0100

    Setup and initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Loader, Navbar, Footer, Transactions, Welcome, Services } from "./components"
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
